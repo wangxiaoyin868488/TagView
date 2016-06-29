e2434eb91b1b573d987ea68041f8e0b207bf1bbb
wangningbo <wangningbo@beibeiapp.cn>
2016-06-29 11:34:12 +0800

    Initial Commit

---
+++ TagView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TagView
+//
+//  Created by apple on 16/6/29.
+//  Copyright © 2016年 白玉京. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
